043763f54ca81d85d1a3454623950ca19f0b288e
xialeidns@gmail.com <xialeidns@gmail.com>
2017-05-20 23:22:52 +0800

    fix some layout

--- nav/resources/views/index/index.blade.php
+++ nav/resources/views/index/index.blade.php
@@ -15,17 +15,30 @@
 
 </div>
 <div class="row">
-    <div class="col-md-1">2</div>
-    <div class="col-md-7">
-        <div class="jumbotron">
-            <ul><li class="site-multiple"><a href="https://www.baidu.com/index.php?tn=monline_3_dg" title="百度">百度</a><a class="site-sub first" href="http://tieba.baidu.com/" title="贴吧">贴吧</a></li><li class="site-multiple site-dropdown"><a href="http://www.sina.com.cn/" title="新 浪" style="width:45px" class="dropdown-menu">新 浪</a><div class="site-dropmenu"><a class="site-sub first" href="http://news.sina.com.cn/" title="新浪新闻">新浪新闻</a><a class="site-sub" href="http://sports.sina.com.cn/" title="新浪体育">新浪体育</a><a class="site-sub" href="http://finance.sina.com.cn/" title="新浪财经">新浪财经</a><a class="site-sub" href="http://mil.news.sina.com.cn/" title="新浪军事">新浪军事</a></div></li><li><a href="http://weibo.com/?c=spr_web_sq_firefox_weibo_t001" title="微　博">微　博</a></li><li class="site-multiple site-dropdown"><a href="http://www.qq.com/" title="腾 讯" style="width:45px" class="dropdown-menu">腾 讯</a><div class="site-dropmenu"><a class="site-sub first" href="http://v.qq.com/" title="腾讯视频">腾讯视频</a><a class="site-sub" href="https://wx.qq.com/" title="网页微信">网页微信</a><a class="site-sub" href="http://qzone.qq.com/" title="QQ空间">QQ空间</a><a class="site-sub" href="http://pc.qq.com/" title="软件中心">软件中心</a></div></li><li><a href="http://www.163.com/" title="网　易">网　易</a></li><li><a href="http://www.ifeng.com/" title="凤 凰 网">凤 凰 网</a></li><li><a href="http://www.sohu.com/" title="搜 狐 网">搜 狐 网</a></li><li><a href="http://www.firefoxchina.cn/" title="火狐中文网">火狐中文网</a></li><li><a href="http://www.zhihu.com/" title="知 乎">知 乎</a></li><li><a href="https://c.duomai.com/track.php?site_id=347&amp;aid=3720&amp;euid=&amp;t=http%3A%2F%2Fvip.iqiyi.com%2F" title="爱 奇 艺">爱 奇 艺</a></li><li class="site-multiple"><a href="http://cps.youku.com/redirect.html?id=00000292" title="优酷">优酷</a><a class="site-sub first" href="http://www.tudou.com/" title="土豆">土豆</a></li><li><a href="http://www.autohome.com.cn/" title="汽车之家">汽车之家</a></li><li><a href="http://www.xinhuanet.com/" title="新 华 网">新 华 网</a></li><li><a href="http://www.4399.com/" title="4 3 9 9">4 3 9 9</a></li></ul>
+    <div class="col-xs-12 col-md-1">2</div>
+    <div  class="col-xs-12 col-md-7 section_content top_nav ">
+        <div class="col-md-12 url_nav">
+            <ul>
+                <li><a>百度</a></li>
+                <li><a>新浪</a></li>
+                <li><a>淘宝</a></li>
+                <li><a>安居客</a></li>
+            </ul>
+            <hr />
+
+            <ul>
+                <li><a>百度</a></li>
+                <li><a>新浪</a></li>
+                <li><a>淘宝</a></li>
+                <li><a>安居客</a></li>
+            </ul>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="jumbotron">
-        </div>
+    <div class="col-md-3 section_content top_nav">
+
     </div>
     <div>4</div>
+
 </div>
 
 
